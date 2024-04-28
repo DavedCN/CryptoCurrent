4221df8134b32a6d1086bd3eb32d2e9240987684
DavedCN <davednnadozie@gmail.com>
2024-04-28 12:12:43 +0100

    Floating Image with Framer motion

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,6 +1,8 @@
 import iphone from "../../assets/iphone.png";
 import gradient from "../../assets/gradient.png";
 
+import { delay, motion } from "framer-motion";
+
 import { shareData } from "../../functions/sharedata";
 
 import Button from "../Common/Button/Button";
@@ -16,25 +18,55 @@ const LandingPage = () => {
   return (
     <div className="home-container">
       <div className="text-section">
-        <h1>Track Crypto </h1>
-        <h2 className="blue">Real Time .</h2>
-        <p>
+        <motion.h1
+          initial={{ opacity: 0, x: 50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          Track Crypto{" "}
+        </motion.h1>
+        <motion.h2
+          initial={{ opacity: 0, x: 50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5, delay: 0.5 }}
+          className="blue"
+        >
+          Real Time .
+        </motion.h2>
+        <motion.p
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.7 }}
+        >
           Track crypto through a public api in real time. Visit the dashboard to
           do so!
-        </p>
-        <div className="buttons">
+        </motion.p>
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 1 }}
+          className="buttons"
+        >
           <Button link={"/dashboard"} text={"Dashboard"} />
           <Button
             className="landingpagebtn"
             text={"Share App"}
             func={handleShare}
           />
-        </div>
+        </motion.div>
       </div>
       <div className="image-section">
-        <img
+        <motion.img
           src={iphone}
           alt="aCrypto tracking app interface"
+          initial={{ y: -25 }}
+          animate={{ y: 25}}
+          transition={{
+            type: "smooth",
+            repeatType: "mirror",
+            duration: 1.5,
+            repeat: Infinity,
+          }}
           className="iphone"
         />
         <img

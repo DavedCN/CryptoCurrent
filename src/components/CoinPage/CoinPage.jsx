53ba1471c36e9bea189380fc0346e2c7d0ede2b1
DavedCN <davednnadozie@gmail.com>
2024-05-02 15:08:39 +0100

    x

--- src/components/CoinPage/CoinPage.jsx
+++ src/components/CoinPage/CoinPage.jsx
@@ -14,6 +14,13 @@ const CoinPage = () => {
   const [days, setDays] = useState(30);
   const [priceType, setPriceType] = useState("prices");
   const [multiAxis, setMultiAxis] = useState(false);
+  const handleDaysChange = (e) => {
+    setDays(parseInt(e.target.value));
+  };
+
+  const handlePriceType = (type) => {
+    setPriceType(type);
+  };
 
   const { id } = useParams();
 
@@ -23,7 +30,7 @@ const CoinPage = () => {
       coinObject(setCoinData, data);
 
       const chartData = await fetchChartData(id, days);
-      console.log(chartData.prices);
+      console.log(chartData);
 
       if (coinData) {
         const labels = [];
@@ -48,18 +55,41 @@ const CoinPage = () => {
     };
 
     fetchData();
-  }, [id]);
+  }, [id, days, priceType]);
+
+  console.log(chartData);
   return (
     <Fragment>
+      <div className="options">
+        <div className="dropDown">
+          <select value={days} onChange={handleDaysChange}>
+            <option value={30}>30 days</option>
+            <option value={90}>90 days</option>
+            <option value={180}>180 days</option>
+            <option value={365}>365 days</option>
+          </select>
+        </div>
+        <div className="priceType">
+          <button onClick={() => handlePriceType("prices")}>Prices</button>
+          <button onClick={() => handlePriceType("market_caps")}>
+            Market Cap
+          </button>
+          <button onClick={() => handlePriceType("total_volumes")}>
+            Volume
+          </button>
+        </div>
+      </div>
+
       <div className="grey-wrapper">
+        {" "}
         {coinData?.length !== 0 ? <List coin={coinData} /> : <Loader />}
       </div>
+
       {chartData && (
         <div className="grey-wrapper">
           <LineChart chartData={chartData} multiAxis={multiAxis} />
         </div>
       )}
-
       <CoinInfo heading={coinData?.name} desc={coinData?.desc} />
     </Fragment>
   );
